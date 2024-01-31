843807b530bbe6a9a1d442c82483050245bf1799
EltexHomeworkLevinDD <pykanus172@gmail.com>
2024-01-31 20:57:57 +0700

    work + screenshots + улучшил комментарии

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
         perror(msg); \
         exit(EXIT_FAILURE); } while(0)
 
-// Всегда постоянные параметры
+// Всегда постоянные параметры (и для исходников библиотек)
 #define L_NAME_LENGTH           127   // Точная длина имени библиотеки (без учета '\0')
 #define L_FUNCTION_NAME_LEN     127   // Точная длина имени функции в библиотеке
 #define L_DESCRIPLION_LENGTH    127   // Точная длина описания функции
@@ -21,22 +21,22 @@
     2) Глобальная строковая переменная размером 128 символов (127 символов без учета '\0'), под названием [library]_DESCRIPTION, содержащая описание функции
 */
 
-#define L_NAME_PREFIX           "./lib/lib"             // Префикс в имени библиотеки [directory]/[lib]
+#define L_NAME_PREFIX           "./lib/lib"             // Префикс в имени библиотеки [directory]/[prefix][library][postfix] 
 #define L_NAME_PREFIX_LEN       9                       // Длина префикса в имени библиотеки (длина L_NAME_PREFIX без учета '\0')
-#define L_NAME_POSTFIX          ".so"                   // Постфикс в имени библиотеки [directory]/[lib]           
-#define L_NAME_POSTFIX_LEN      3                       // Длина Постфикс в имени библиотеки (длина L_NAME_PREFIX без учета '\0')
+#define L_NAME_POSTFIX          ".so"                   // Постфикс в имени библиотеки [directory]/[prefix][library][postfix]      
+#define L_NAME_POSTFIX_LEN      3                       // Длина постфикса в имени библиотеки (длина L_NAME_PREFIX без учета '\0')
 #define L_FUNCTION_NAME_POSTFIX  "_FUNCTION_NAME"       // Постфикс переменной, содержащей имя функции
 #define L_DESCRIPTION_NAME_POSTFIX  "_DESCRIPTION"      // Постфикс переменной, содержащей описание функции
 #define MAX_L_COUNT             5                       // Максимальное количество библиотек
 
 // Структура библиотеки
 struct Library{
-    void* handle;                                                   // Указатель на библиотеку
-    const char short_name[L_NAME_LENGTH+1];                     // Короткое имя (без префикса и постфикса)
-    int (*function)(int,int);                                       // Указатель на функцию
-    const char function_name[L_FUNCTION_NAME_LEN+1];                // Имя функции в библиотеке
-    const char description[L_DESCRIPLION_LENGTH+1];               // Описание функции
-    const char full_name[L_NAME_PREFIX_LEN+L_NAME_LENGTH+L_NAME_POSTFIX_LEN+1];    // Полное имя библиотеки (с префиксом и постфиксом)
+    void* handle;                                       // Указатель на библиотеку
+    const char short_name[L_NAME_LENGTH+1];             // Короткое имя библиотеки (без префикса и постфикса)
+    int (*function)(int,int);                           // Указатель на функцию библиотеки
+    const char function_name[L_FUNCTION_NAME_LEN+1];    // Имя функции в библиотеке
+    const char description[L_DESCRIPLION_LENGTH+1];     // Описание функции в библиотеке
+    const char full_name[L_NAME_PREFIX_LEN+L_NAME_LENGTH+L_NAME_POSTFIX_LEN+1]; // Полное имя библиотеки (с префиксом и постфиксом)
 };
 
 void input_data(int *value1, int *value2);
@@ -48,12 +48,12 @@ int main(){
 
     struct Library libraries[MAX_L_COUNT];          // Создаем массив, содержащий библиотеки
     unsigned int libraries_count = 0;               // Действительное количетсво библиотек
-    memset(libraries, '\0', sizeof(libraries));     // Зануляем поля
+    memset(libraries, '\0', sizeof(libraries));     // Зануляем поля структуры
 
     char lib_full_name[L_NAME_PREFIX_LEN+L_NAME_LENGTH+L_NAME_POSTFIX_LEN+1]; // Строковая переменная полного имени библиотеки
     char lib_short_name[L_NAME_LENGTH+1];               // Строковая переменная короткого имени библиотеки
-    char function_name_var[L_FUNCTION_NAME_LEN + 1];    // Имя строковой переменной с именем функции
-    char description_var[L_DESCRIPLION_LENGTH+1];       // Имя строковой переменной с описанием функции
+    char function_name_var[L_FUNCTION_NAME_LEN + 1];    // Имя строковой переменной, содержащей имя функции
+    char description_var[L_DESCRIPLION_LENGTH+1];       // Имя строковой переменной, содержащей описание функции
 
     while (libraries_count < MAX_L_COUNT){ 
         // Зануляю строки
@@ -64,21 +64,21 @@ int main(){
         // Считать строку
         printf("\t> ");
         if (fgets(lib_short_name, L_NAME_LENGTH+1, stdin) == NULL)   // Считываю строку
-            EXITMSG("Error reading library name\n");
-        // Если пользователь вышел, закрыть ввод
+            EXITMSG("Error entering library name\n");
+        // Если пользователь ввел выход, закрыть ввод
         if (!strcmp(lib_short_name, "q\n")){ 
             break;
         }
-        int len = strcspn(lib_short_name, "\n");  // Найти позицию символа новой строки (после fgets)
-        lib_short_name[len] = '\0';  // Заменить символ новой строки нулевым символом
-        // Копируем строку char с коротким именем библиотеки в структуру с полем const char
+        int len = strcspn(lib_short_name, "\n");    // Найти позицию символа новой строки (после fgets)
+        lib_short_name[len] = '\0';                 // Заменить символ новой строки нулевым символом
+        // Копируем строку char с коротким именем библиотеки в поле структуры с const char короткого имени библиотеки
         memcpy((void*)libraries[libraries_count].short_name, (void*)lib_short_name, sizeof(lib_short_name)); 
         
-        // Создать строку полного имени
+        // Создать строку полного имени библиотеки
         strcpy(lib_full_name, L_NAME_PREFIX);
         strcat(lib_full_name, lib_short_name);
         strcat(lib_full_name, L_NAME_POSTFIX);
-        // Копипуем строку char полного имени библиотеки в структуру с полем const char 
+        // Копипуем строку char полного имени библиотеки в поле структуры с const char 
         memcpy((void*)libraries[libraries_count].full_name, (void*)lib_full_name, sizeof(lib_full_name)); 
 
         // Загружаем библиотеку
@@ -88,21 +88,21 @@ int main(){
             EXITMSG("");
         } 
 
-        // Определяем имя переменной с именем функции
+        // Определяем имя переменной, содержащей имя функции
         strcat(function_name_var, lib_short_name);
         strcat(function_name_var, L_FUNCTION_NAME_POSTFIX);
 
-        // Получили переменную с именем функции
+        // Получаем переменную, содержащую имя функции
         void* function_name_var_ptr = dlsym(libraries[libraries_count].handle, (const char*)function_name_var);
         if (function_name_var_ptr == NULL) {
             printf("Error loading the global virable FUNCTION_NAME: %s\n", dlerror());
             dlclose(libraries[libraries_count].handle);
             EXITMSG("");
         }
-        // Копируем char имя функции в структуру с полем const char 
+        // Копируем строку char с именем функции в поле структуры с const char 
         memcpy((void*)libraries[libraries_count].function_name, (void*)function_name_var_ptr, L_FUNCTION_NAME_LEN); 
 
-        // Получаем void указатель на функцию
+        // Получаем void указатель на функцию библиотеки
         libraries[libraries_count].function = dlsym(libraries[libraries_count].handle, function_name_var_ptr);
         if (libraries[libraries_count].function == NULL) {
             printf("Error loading the function: %s\n", dlerror());
@@ -110,22 +110,23 @@ int main(){
             EXITMSG("");
         }
 
-        // Определяем имя переменной с описанем функции
+        // Определяем имя переменной, содержащей описание функции
         strcat(description_var, lib_short_name);
         strcat(description_var, L_DESCRIPTION_NAME_POSTFIX);
 
-        // Получили переменную с описанем функции
+        // Получаем переменную, содержащую описание функции
         void* description_var_ptr = dlsym(libraries[libraries_count].handle, (const char*)description_var);
         if (description_var_ptr == NULL) {
             printf("Error loading the global virable DESCRIPTION: %s\n", dlerror());
             dlclose(libraries[libraries_count].handle);
             EXITMSG("");
         }
-        // Копируем char описане функции в структуру с полем const char 
+        // Копируем char описание функции в поле структуры с const char 
         memcpy((void*)libraries[libraries_count].description, (void*)description_var_ptr, L_DESCRIPLION_LENGTH); 
         libraries_count++;
     }
 
+    // Выводим меню команд
     printf("==== Calculator ====\n");
     printf("--- Command menu ---\n");
     printf("0 - exit             \n");
@@ -134,7 +135,7 @@ int main(){
     }
     printf("--------------------\n");
 
-
+    // Считываем команды
     unsigned int command = 0;
     int value1 = 0, value2 = 0;
     int work = 1;
@@ -144,20 +145,25 @@ int main(){
         printf("cmd --> ");
         scanf("%u", &command);
 
+        // Если введена недоступная команда
         if (command > libraries_count){
             printf("\tWrong command\n");
         }else{
+            // Если введена команда выхода
             if (command == 0){
                 printf("\tExit\n");
                 work = 0;
             }else{
+                // Если введена команда выполнения
                 input_data(&value1, &value2);
                 printf("\tresult: %d\n", libraries[command-1].function(value1, value2));
             }
         }
     }
 
+    // Закрыть библиотеки
     for (int i = 0; i < libraries_count; i++){
+        // Если закрыть не удалось
         if(dlclose(libraries[i].handle)){
             printf("Error closing the library: %s\n", dlerror());
         }
